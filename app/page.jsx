d0156084118c9248870ac5355de21927c1c875b2
midCenturyDude7 <mgriffes@gmail.com>
2023-05-06 14:01:42 -0700

    Working on Home Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "../components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -10,9 +12,8 @@ const Home = () => {
         Promptopia is an open-source AI prompting tool that allows you to
         discover, create and share prompts.
       </p>
-      
-      {/* Feed */}
-      
+
+      <Feed />
     </section>
   );
 };
