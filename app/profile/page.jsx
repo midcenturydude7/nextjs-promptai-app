21d6625085928c7128adb036ef354db241fe74ec
midCenturyDude7 <mgriffes@gmail.com>
2023-05-20 15:14:59 -0700

    Working on profile page and populating with posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
 import React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = React.useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/posts`);
+    const data = await response.json();
+
+    setPosts(data);
+  };
+
+  React.useEffect(() => {
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 "use client";
@@ -7,7 +8,17 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = React.useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  };
 
   return (
     <div className="prompt_card">
@@ -29,8 +40,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             </p>
           </div>
         </div>
-
-        <div className="copy_btn">
+        {/* TODO: This needs to become a button element */}
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
@@ -49,6 +60,21 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         onClick={() => handleTagClick && handleTagClick(post.tag)}>
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}>
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
